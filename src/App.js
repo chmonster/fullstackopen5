b4d59d9112d5e0bfededb2a5715323298bd281f1
chmonster <perry.moore@gmail.com>
2022-05-13 16:08:36 -0230

    5.1-5.2 complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,88 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newTitle, setNewTitle] = useState('')
+  const [newUrl, setNewUrl] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  
+
+  const handleTitleChange = (event) => {
+    setNewTitle(event.target.value)
+  }
+
+  const handleUrlChange = (event) => {
+    setNewUrl(event.target.value)
+  }
+  const handleAuthorChange = (event) => {
+    setNewAuthor(event.target.value)
+  }
+
+  const handleLogin =async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+      setErrorMessage(null)
+      }, 5000)
+    } 
+  }
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    console.log('logging out with', user.name)
+    try {
+      window.localStorage.removeItem('loggedBlogappUser')
+      blogService.setToken(null)
+      setUser(null)
+      setUsername('')
+      setPassword('')         
+    } catch {
+      setErrorMessage('Logout Error')
+      setTimeout(() => {
+      setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl,
+    }
+
+    blogService
+      .create(blogObject)
+      .then(returnedBlog => {
+        setBlogs(blogs.concat(returnedBlog))
+        setNewTitle('')
+        setNewUrl('')
+        setNewAuthor('')
+      })
+  }
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +90,74 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+      <br /> 
+    </form>
+  )
+  
+  const blogForm = () => (
+    <>
+      <p>
+        {user.name} logged in 
+        <button onClick={handleLogout}>Log out</button>
+      </p>
+      <form onSubmit={addBlog}>
+        Title
+        <input
+          value={newTitle}
+          onChange={handleTitleChange}
+        />
+        URL
+        <input
+          value={newUrl}
+          onChange={handleUrlChange}
+        />
+        Author
+        <input
+          value={newAuthor}
+          onChange={handleAuthorChange}
+        />
+        <button type="submit">save</button>
+      </form>  
+    </>
+  )
+
   return (
     <div>
       <h2>blogs</h2>
+      <Notification message={errorMessage} />
+      {user === null && loginForm()}
+      {user !== null && blogForm()}
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,12 @@
 const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
+  <table style={{width: '50%'}}>
+    <tr>
+      <td><a href={blog.url} title={blog.title} alt={blog.title}>
+        {blog.title}
+      </a></td> 
+      <td style={{textAlign:'right'}}> {blog.author} </td>
+    </tr>
+  </table>
 )
 
 export default Blog
